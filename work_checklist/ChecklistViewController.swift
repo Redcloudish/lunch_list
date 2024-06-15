43469e85906a1b479d8bb82549cdb33fa8d9e969
Michael Brown <160866598+Redcloudish@users.noreply.github.com>
2024-06-15 19:45:45 -0400

    almost done

--- work_checklist/ChecklistViewController.swift
+++ work_checklist/ChecklistViewController.swift
@@ -7,60 +7,96 @@
 
 import UIKit
 
-class ChecklistViewController: UITableViewController {
-    
+class ChecklistViewController: UITableViewController, AddItemViewControllerDelegate {
+    var items = [ChecklistItem]()
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-    
-    override func tableView(
-        _ tableView: UITableView,
-        numberOfRowsInSection section: Int
-    ) -> Int {
-        return 5
-    }
-    
-    override func tableView(
-        _ tableView: UITableView,
-        cellForRowAt indexPath: IndexPath
-    ) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(
-            withIdentifier: "ChecklistItem",
-            for: indexPath)
-        // Add the following code
-        let label = cell.viewWithTag(1000) as! UILabel
         
-        if indexPath.row == 0 {
-            label.text = "Strawberry Splash"
-        } else if indexPath.row == 1 {
-            label.text = "Chilly Soup"
-        } else if indexPath.row == 2 {
-            label.text = "Iced Coffee"
-        } else if indexPath.row == 3 {
-            label.text = "Flatbread Pizza"
-        } else if indexPath.row == 4 {
-            label.text = "Vanilla Latte"
+        // Add the initial checklist items
+        let item1 = ChecklistItem()
+        item1.text = "Chilly soup"
+        items.append(item1)
+
+        let item2 = ChecklistItem()
+        item2.text = "Strawberry splash"
+        
+        items.append(item2)
+
+        let item3 = ChecklistItem()
+        item3.text = "Iced Coffee"
+        item3.checked = true
+        items.append(item3)
+
+        let item4 = ChecklistItem()
+        item4.text = "BLT Sandwich"
+        items.append(item4)
+
+        let item5 = ChecklistItem()
+        item5.text = "Pack of Timbits"
+        items.append(item5)
+
+        // Turn off large titles
+        navigationController?.navigationBar.prefersLargeTitles = true
+    }
+
+    // MARK: - Add Item ViewController Delegates
+    func addItemViewControllerDidCancel(_ controller: AddItemViewController) {
+        navigationController?.popViewController(animated: true)
+    }
+
+    func addItemViewController(_ controller: AddItemViewController, didFinishAdding item: ChecklistItem) {
+        let newRowIndex = items.count
+        items.append(item)
+
+        let indexPath = IndexPath(row: newRowIndex, section: 0)
+        let indexPaths = [indexPath]
+        tableView.insertRows(at: indexPaths, with: .automatic)
+        navigationController?.popViewController(animated: true)
+    }
+
+    // MARK: - Navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "AddItem" {
+            let controller = segue.destination as! AddItemViewController
+            controller.delegate = self
         }
-        // End of new code block
-        
+    }
+
+    // MARK: - Table View Data Source
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ChecklistItem", for: indexPath)
+        let item = items[indexPath.row]
+
+        configureText(for: cell, with: item)
+        configureCheckmark(for: cell, with: item)
+
         return cell
     }
-    // MARK: - Table View Delegate
-    override func tableView(
-        _ tableView: UITableView,
-        didSelectRowAt indexPath: IndexPath
-    ) {
-        if let cell = tableView.cellForRow(at: indexPath) {
-            if cell.accessoryType == .none {
-                cell.accessoryType = .checkmark
-            } else {
-                cell.accessoryType = .none
-            }
+
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            items.remove(at: indexPath.row)
+            let indexPaths = [indexPath]
+            tableView.deleteRows(at: indexPaths, with: .automatic)
         }
-        
-        tableView.deselectRow(at: indexPath, animated: true)
+    }
+
+    // MARK: - Helper Methods
+    func configureCheckmark(for cell: UITableViewCell, with item: ChecklistItem) {
+        if item.checked {
+            cell.accessoryType = .checkmark
+        } else {
+            cell.accessoryType = .none
+        }
+    }
+
+    func configureText(for cell: UITableViewCell, with item: ChecklistItem) {
+        let label = cell.viewWithTag(1000) as! UILabel
+        label.text = item.text
     }
 }
-
-
---
+++ work_checklist/work_Checklistitem.swift
@@ -0,0 +1,12 @@
+//
+//  work_Checklistitem.swift
+//  work_checklist
+//
+//  Created by mike brown on 2024-06-15.
+//
+
+import Foundation
+class ChecklistItem {
+  var text = ""
+  var checked = false
+}
